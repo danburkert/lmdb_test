9225f6675ef9a02ab426364a0ad3b055a86d2a20
Dan Burkert <dan@danburkert.com>
2017-09-01 08:49:37 -0700

    Further simplify the example

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,54 @@
-extern crate bincode;
-extern crate lmdb as libmdb;
-extern crate rayon;
-#[macro_use]
-extern crate serde_derive;
+extern crate lmdb;
 extern crate tempdir;
+extern crate byteorder;
 
-mod lmdb;
+use std::path::Path;
 
+use byteorder::{ByteOrder, LittleEndian};
+use lmdb::{Database, Environment, Transaction};
 use tempdir::TempDir;
-use rayon::prelude::*;
-use lmdb::LMDB;
+
+const HEIGHT_KEY: [u8; 1] = [0];
 
 fn main() {
     let dir = TempDir::new("test").unwrap();
     let db = LMDB::new(dir.path(), 1_000_000_000);
 
-    db.clear();
+    for height in 0..100000 {
+        eprintln!("height: {}", height);
+        db.save_height(height);
+    }
+}
 
-    let height: Vec<_> = (1..100000).collect();
+#[derive(Debug)]
+pub struct LMDB {
+    pub env: Environment,
+    pub index: Database,
+}
 
-    height
-        .iter()
-        .map(|height| { db.save_height(*height); })
-        .collect::<Vec<()>>();
+impl LMDB {
+    // add code here
+    pub fn new(path: &Path, size: usize) -> Self {
+        let mut builder = Environment::new();
+        builder.set_max_dbs(2);
+        builder.set_map_size(size);
+        let env = builder.open(path).expect("open lmdb env");
+        let index = env.create_db(None, lmdb::DUP_SORT).expect("open index db");
+
+        LMDB {
+            env: env,
+            index: index,
+        }
+    }
+
+    pub fn save_height(&self, height: u64) {
+        let mut value = [0u8; 8];
+        LittleEndian::write_u64(&mut value, height);
+        let mut tx = self.env.begin_rw_txn().expect("begin_rw_txn");
+        tx.put(self.index,
+               &HEIGHT_KEY,
+               &value,
+               lmdb::WriteFlags::empty()).expect("tx.put");
+        tx.commit().expect("ts.commit")
+    }
 }
